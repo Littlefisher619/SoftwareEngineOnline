4b47b53fde6ec55aabed22176e9e040393cac69a
Littlefisher619 <619815617@qq.com>
2020-09-06 21:31:29 +0800

    user center

--- backend/urls.py
+++ backend/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from .views.authviews import *
+from .views.userviews import UserViewSetNormal
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register(r'signup', SignUpView, basename='注册')
-router.register(r'login', LoginView, basename='登录', )
+router.register(r'login', LoginView, basename='登录')
+router.register(r'user', UserViewSetNormal, basename='用户中心')
 
 
 urlpatterns = [
