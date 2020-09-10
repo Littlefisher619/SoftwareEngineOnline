ebce3a8ace3b5276e7ab880a95b0e272441a7469
Littlefisher619 <619815617@qq.com>
2020-09-10 18:25:05 +0800

    judgement create

--- backend/serializers/judgementserializers.py
+++ backend/serializers/judgementserializers.py
@@ -1,12 +1,35 @@
 from rest_framework import serializers
 from rest_framework.fields import ChoiceField
 
-from backend.models import Judgement, Group
+from backend.models import Judgement, Group, HomeWork
+from .authserializers import UserInfoSerializer
+from .homworkserializer import HomeWorkSerializer
 from .jsonserializer import JsonSerializer
 
 
 class JudgementInfoSerializer(serializers.ModelSerializer):
     scoredatail = JsonSerializer(label='评分详情')
+    judger = UserInfoSerializer(label='评分员', read_only=True)
+    homework = HomeWorkSerializer(label='作业')
+
+    def validate(self, attrs):
+        super().validate(attrs)
+        print(attrs)
+        if attrs.get('homework') is None:
+            raise serializers.ValidationError("需指定一个作业才能进行评分")
+
+        if attrs.get('homework').homeworktype == HomeWork.SIGNGLE:
+            if not attrs.get('student'):
+                raise serializers.ValidationError("作业为单人作业时，必须指定student关键字以创建评分对象")
+            if attrs.get('group'):
+                raise serializers.ValidationError("作业为单人作业时，只能指定student关键字")
+
+        if attrs.get('homework').homeworktype in [HomeWork.GROUP, HomeWork.DOUBLE]:
+            if not attrs.get('group'):
+                raise serializers.ValidationError("作业为结对/团队作业时，必须指定group关键字以创建评分对象")
+            if attrs.get('student'):
+                raise serializers.ValidationError("作业为结对/团队作业时，只能指定group关键字")
+        return attrs
 
     class Meta:
         model = Judgement
--- backend/views/judgementviews.py
+++ backend/views/judgementviews.py
@@ -12,7 +12,6 @@ class JudgementViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Re
     queryset = Judgement.objects.all()
     permission_classes = [IsAuthenticated, ]
 
-
     @action(methods=['POST'], url_path='create', detail=False)
     def _create(self, request):
         if self.request.user.role != User.TEST_GROUP:
@@ -20,37 +19,20 @@ class JudgementViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Re
                     'success': False,
                     'message': '不是测试组的成员不能创建评分诶'
                 }, status=status.HTTP_200_OK)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # serializer.validated_data['']
-
-
-
-        # if Group.objects.filter(leader=self.request.user, grouptype=serializer.validated_data['grouptype']).exists():
-        #     return Response({
-        #         'success': False,
-        #         'message': '你已经是当前队伍类型的组长了，不能再创建新的队伍'
-        #     }, status=status.HTTP_200_OK)
-        #
-        # for data in Group.objects.filter(grouptype=serializer.validated_data['grouptype']).values('members'):
-        #     if self.request.user.pk in json.loads(data['members']):
-        #         return Response({
-        #             'success': False,
-        #             'message': '你已经是当前队伍类型的某个组的组员，不能再创建新的队伍'
-        #         }, status=status.HTTP_200_OK)
-        #
-        #
+        # user, group = student,
         self.perform_create(serializer)
-        # return Response(
-        #     {
-        #         'success': True,
-        #         'message': '创建成功',
-        #         'data': serializer.data
-        #     }, status=status.HTTP_201_CREATED
-        # )
+        return Response(
+            {
+                'success': True,
+                'message': '创建成功',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED
+        )
 
     def perform_create(self, serializer):
         serializer.validated_data['judger'] = self.request.user
-        serializer.validated_data['members'] = json.dumps([self.request.user.pk,])
         serializer.save()
 
