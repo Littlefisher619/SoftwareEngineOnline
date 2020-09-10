03955ad1025b7afdcfae1bf32504dff33afa75e7
Littlefisher619 <619815617@qq.com>
2020-09-10 18:01:59 +0800

    get judgement task by homework_id

--- backend/views/homwworkview.py
+++ backend/views/homwworkview.py
@@ -1,13 +1,17 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from backend.serializers.authserializers import UserInfoSerializer
+from backend.serializers.groupserializers import GroupInfoSerializer
 from backend.serializers.homworkserializer import HomeWorkSerializer
-from backend.models import HomeWork
+from backend.models import HomeWork, User, Judgement, Group
 
 
-class HomeWorkViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class HomeWorkViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
     permission_classes_by_action = {
         'create': [IsAdminUser, ],
-        'list': [IsAuthenticated, ],
     }
     serializer_class = HomeWorkSerializer
     queryset = HomeWork.objects.all()
@@ -18,4 +22,41 @@ class HomeWorkViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Cre
     def perform_create(self, serializer):
         serializer.validated_data['author'] = self.request.user
         serializer.save()
+
+    @action(methods=['GET'], url_path='tasklist', detail=True)
+    def tasklist(self, request, pk=None):
+        if self.request.user.role != User.TEST_GROUP:
+            return Response({
+                    'success': False,
+                    'message': '你走错地方惹，这儿啥也没有~'
+                }, status=status.HTTP_200_OK)
+
+        homework = self.get_object()
+        tasklist = []
+        serializer_class = None
+        if homework.homeworktype == homework.SIGNGLE:
+            serializer_class = UserInfoSerializer
+            judgements = Judgement.objects.filter(homework=homework).values_list('student')
+            users = User.objects.all()
+            for user in users:
+                if not judgements.filter(student=user).exists():
+                    tasklist.append(user)
+        elif homework.homeworktype in [homework.GROUP, homework.DOUBLE]:
+            serializer_class = GroupInfoSerializer
+            judgements = Judgement.objects.filter(homework=homework).values_list('student')
+            groups = Group.objects.filter(grouptype=homework.homeworktype)
+            for group in groups:
+                if not judgements.filter(group=group).exists():
+                    tasklist.append(group)
+
+        queryset = self.filter_queryset(tasklist)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = serializer_class(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = serializer_class(queryset, many=True)
+        return Response(serializer.data)
+        pass
 
