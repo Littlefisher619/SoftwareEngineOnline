8bb98470d2abad784f55afd2a590d0ee9dc19d9e
Littlefisher619 <619815617@qq.com>
2020-09-20 20:37:35 +0800

    api for get user's group

--- backend/views/userviews.py
+++ backend/views/userviews.py
@@ -1,8 +1,11 @@
+import json
+
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
+from backend.serializers.groupserializers import GroupInfoSerializer
 from backend.serializers.userserializers import *
 
 
@@ -38,6 +41,52 @@ class UserViewSetNormal(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixi
         instance = self.request.user
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+    def __get_group_info_json_data(self, user):
+        double_group = None
+        big_group = None
+
+        try:
+            double_group = Group.objects.get(leader=user, grouptype=Group.DOUBLE)
+        except Group.DoesNotExist:
+            for group in Group.objects.filter(grouptype=Group.DOUBLE):
+
+                if self.request.user.pk in json.loads(group.members):
+                    double_group = group
+                    break
+
+        try:
+            big_group = Group.objects.get(leader=user, grouptype=Group.GROUP)
+        except Group.DoesNotExist:
+            for group in Group.objects.filter(grouptype=Group.GROUP):
+                if self.request.user.pk in json.loads(group.members):
+                    big_group = group
+                    break
+
+        data = {}
+
+        data['double'] = None if double_group is None else GroupInfoSerializer(double_group).data
+        data['big'] = None if big_group is None else GroupInfoSerializer(big_group).data
+        return data
+
+    @action(methods=['GET'], detail=True, name='组队查询')
+    def groupinfo(self, request, pk):
+        if self.request.user.role != User.TEST_GROUP:
+            return Response({
+                    'success': False,
+                    'message': '你走错地方惹，这儿啥也没有~'
+                }, status=status.HTTP_200_OK)
+        return Response(
+            self.__get_group_info_json_data(self.get_object()),
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['GET'], detail=False, name='我的组队')
+    def mygroup(self, request):
+        return Response(
+            self.__get_group_info_json_data(self.request.user),
+            status=status.HTTP_200_OK
+        )
 
     @action(methods=['POST'], detail=False, name='修改密码')
     def updatepassword(self, request):
