302d49cb86de33c6c176d1e422470feaf4b92ad3
Littlefisher619 <619815617@qq.com>
2020-09-25 18:21:06 +0800

    cors config

--- SoftwareEngineOnline/settings.py
+++ SoftwareEngineOnline/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'SoftwareEngineOnline.urls'
@@ -135,6 +136,10 @@ JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }
 
+# CORS_ORIGIN_WHITELIST = (
+#     '127.0.0.1:8080',
+#     'localhost:8080',
+# )
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 AUTH_USER_MODEL = "backend.User"
