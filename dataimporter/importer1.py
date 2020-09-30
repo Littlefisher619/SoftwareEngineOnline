694c116ff41d586adc6ab9b29e5a707a2cc52eed
Littlefisher619 <619815617@qq.com>
2020-09-30 17:00:40 +0800

    support to import 0-score students

--- dataimporter/importer1.py
+++ dataimporter/importer1.py
@@ -23,42 +23,39 @@ if __name__ == '__main__':
         if i[0] == '':
             continue
         stuid = "%09d" % int(i[0])
-        score = int(i[5])
-        if score == 0:
-            continue
+        score = int(i[5]) if i[5]!='' else 0
         scoredetail = {
             "scorepoints": [],
             "bonus": 0,
-            "score": score,
+            "score": 0,
         }
         for point in range(6, 15):
             scoredetail['scorepoints'].append(
                 {
                     "point": fixed_row[point],
-                    "score": int(i[point]),
+                    "score": int(i[point]) if i[point]!='' else 0,
                 }
             )
+            scoredetail['score'] += int(i[point]) if i[point]!='' else 0
         jsonstr = json.dumps(scoredetail)
         try:
-            userid = User.objects.get(pk=stuid).pk
+            userid = User.objects.get(stuid=stuid).pk
             if Judgement.objects.filter(homework_id=1, student_id=userid).exists():
-                print(stuid, 'judged yet!')
-                continue
-            Judgement(
-                student_id=userid,
-                scoredetail=jsonstr,
-                homework_id=1,
-                judger_id=6,
-            ).save()
+                print(stuid, 'judged yet! Now updating...')
+                judgement = Judgement.objects.get(homework_id=1, student_id=userid)
+                judgement.scoredetail = jsonstr
+                judgement.totalscore = scoredetail['score'] * (1.0 + scoredetail['bonus'])
+                judgement.save()
+            else:
+                Judgement(
+                    student_id=userid,
+                    scoredetail=jsonstr,
+                    totalscore=scoredetail['score'] * (1.0 + scoredetail['bonus']),
+                    homework_id=1,
+                    judger_id=6,
+                ).save()
         except User.DoesNotExist:
             print(stuid, 'is not registered yet!')
-
-
-
-
-
-
-
 
         count += 1
 
