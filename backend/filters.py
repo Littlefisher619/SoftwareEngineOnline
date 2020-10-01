9bd99f40825c78c66e33bc3975166df7f5de2ebe
Littlefisher619 <619815617@qq.com>
2020-10-01 15:29:24 +0800

    search by foreign key value for groupfilter

--- backend/filters.py
+++ backend/filters.py
@@ -6,13 +6,13 @@ from backend.models import *
 class GroupFilter(django_filters.FilterSet):
     class Meta:
         model = Group
-        fields = ("grouptype", "leader", "groupname", "id", )
+        fields = ("grouptype", "leader__stuname", "groupname", "id", "leader")
 
 
 class JudgementFilter(django_filters.FilterSet):
     class Meta:
         model = Judgement
-        fields = ("homework", "judger", "group", "student",)
+        fields = ("homework", "judger", "group", "student", "student__stuname", "student__stuid", "group__leader__stuname")
 
 
 class UserFilter(django_filters.FilterSet):
